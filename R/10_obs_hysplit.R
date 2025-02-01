4256614ec427072833fbf4c6b1d2976c4fced683
sergio <zergioibarra@gmail.com>
2025-02-01 16:33:53 -0700

    fix hysplit asc

--- R/10_obs_hysplit.R
+++ R/10_obs_hysplit.R
@@ -387,7 +387,14 @@ obs_hysplit_ascdata <- function(llc =  c(-90.0, -180.0),
 
   sink(ascdata)
 
-  cat(sprintf("%#.1f", llc), " lat/lon of lower left corner (last record in file)\n", sep = " ")
+  cat(sprintf("%#.1f", llc[1]))
+
+  cat("  ")
+
+  cat(sprintf("%#.1f", llc[2]))
+
+  cat(" lat/lon of lower left corner (last record in file)\n")
+
 
   cat(sprintf("%#.1f", spacing[1]))
 
