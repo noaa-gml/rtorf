361bfab323ee5538ee08d4d2327ca57d2a21f6a2
ibarraespinosa <zergioibarra@gmail.com>
2025-07-16 11:05:17 -0600

    add obs_nc*

--- R/12_obs_nc.R
+++ R/12_obs_nc.R
@@ -1,7 +1,7 @@
 #' obs_nc
 #'
 #' Creates NetCDF based on dimension from another NetCDF
-#' with custom dimensions, and attributtes
+#' with custom dimensions, and attributes
 #'
 #' @param nc_path String pointing to the target NetCDF
 #' @param nc_name String indicating the spatial array
@@ -9,7 +9,7 @@
 #' @param nc_lon String to extract longitude
 #' @param time_nc Time of the NetCDF (length 1).
 #' @param vars_out names for the variables to be created in the NetCDF.
-#' @param units units for the NetCDF variables to be created.
+#' @param units_out units for the NetCDF variables to be created.
 #' @param nc_out path for the created NetCDF.
 #' @param larrays list of arrays, length equal to vars_out.
 #' @param verbose Logical, to display more information.
@@ -17,17 +17,28 @@
 #' @export
 #' @examples {
 #' # Do not run
+#' nc_path <- paste0("Z:/footprints/aircraft/flask/2018",
+#' "/04/hysplit2018x04x08x15x15x38.7459Nx077.5584Wx00594.nc")
+#' foot <- obs_nc_get(nc_path)
+#' nco <- paste0(tempfile(), "2.nc")
+#' file.remove(nco)
+#' obs_nc(nc_path = nc_path,
+#'        time_nc = ISOdatetime(2018, 4, 8, 15, 15, 38),
+#'        vars_out = c("a", "b"),
+#'        nc_out  = nco,
+#'        larrays = list(a = foot, b = foot),
+#'        verbose = TRUE)
 #' }
 obs_nc <- function(nc_path = "AAA",
                    nc_name = "foot1",
                    nc_lat = "foot1lat",
                    nc_lon = "foot1lon",
-                   time_nc = Sys.time(),
+                   time_nc,
                    vars_out = c("total",
-                            "bio",
-                            "ocn",
-                            "fossil",
-                            "fire"),
+                                "bio",
+                                "ocn",
+                                "fossil",
+                                "fire"),
                    units_out = "(ppb/nanomol m-2 s-1)*nanomol m-2 s-1",
                    nc_out,
                    larrays,
@@ -55,7 +66,7 @@ obs_nc <- function(nc_path = "AAA",
     stop("time_nc must be a single value")
   }
 
-  if(verbose) cat(paste0("Reading ", nc_path))
+  if(verbose) cat(paste0("Reading ", nc_path, "\n"))
 
   nc <- ncdf4::nc_open(nc_path)
 
@@ -80,11 +91,19 @@ obs_nc <- function(nc_path = "AAA",
   # lon_vals <- seq(from = 10.5, to = 79.5, by = 1)
   # lat_vals <- seq(from = -29.5, to = 39.5, by = 1)
 
-  lon <- ncdf4::ncdim_def( "longitude", "degreesE", as.double(foot1lat))
-  lat <- ncdf4::ncdim_def( "latitude", "degreesN", as.double(foot1lon))
+  lon <- ncdf4::ncdim_def( "longitude",
+                           "degreesE",
+                           as.double(foot1lat))
 
-  time_back <- ncdf4::ncdim_def( "Time", " seconds since 1970-01-01 00:00:00 UTC",
-                                 as.numeric(time_nc),
+  lat <- ncdf4::ncdim_def( "latitude",
+                           "degreesN",
+                           as.double(foot1lon))
+
+  time_receptor  = rep(time_nc - (dim(foot)[3] - 1)*3600, dim(foot)[3])
+
+  time_back <- ncdf4::ncdim_def( "Time",
+                                 " seconds since 1970-01-01 00:00:00 UTC",
+                                 as.numeric(time_receptor),
                                  unlim=TRUE)
 
   lv <- lapply(seq_along(vars_out), function(l) {
@@ -103,9 +122,9 @@ obs_nc <- function(nc_path = "AAA",
   a <- ncdf4::nc_create(nc_out,
                         vars = lv,
                         force_v4 = TRUE,
-                        verbose = TRUE)
+                        verbose = FALSE)
 
-  for(k in seq_along(vars)) {
+  for(k in seq_along(vars_out)) {
 
     ncdf4::ncvar_put(nc = a,
                      varid = vars_out[k],
@@ -120,8 +139,9 @@ obs_nc <- function(nc_path = "AAA",
                                           format(Sys.time(),
                                                  "%Y-%m-%d at %H:%M")),
                           Author = paste0("R package rtorf v",
-                                          utils::packageVersion("rtorf"))),
-                     g_atributos[4:length(g_atributos)])
+                                          utils::packageVersion("rtorf"),
+                                          " and ncdf4 ",
+                                          utils::packageVersion("ncdf4"))))
 
   for(i in 1:length(g_atributos)){
     ncdf4::ncatt_put(a,
@@ -135,3 +155,68 @@ obs_nc <- function(nc_path = "AAA",
 
 
 }
+
+#' obs_nc_get
+#'
+#' Reads NetCDF var and returns the spatial array
+#'
+#' @param nc_path String pointing to the target NetCDF
+#' @param nc_name String indicating the spatial array
+#' @param nc_lat String to extract latitude.
+#' @param nc_lon String to extract longitude
+#' @param verbose Logical, to display more information.
+#' @return A NetCDF file with the custom attributes and units
+#' @export
+#' @examples {
+#' # Do not run
+#' nc_path <- paste0("Z:/footprints/aircraft/flask/2018/04",
+#' "/hysplit2018x04x08x15x15x38.7459Nx077.5584Wx00594.nc")
+#' f <- obs_nc_get(nc_path = nc_path)
+#' }
+
+obs_nc_get <- function(nc_path = "AAA",
+                       nc_name = "foot1",
+                       nc_lat = "foot1lat",
+                       nc_lon = "foot1lon",
+                       verbose = FALSE
+){ # function to aggregate convolved fluxes
+
+
+  if(length(nc_path) != 1) {
+    stop("nc_path must be a single string")
+  }
+
+  if(length(nc_name) != 1) {
+    stop("nc_name must be a single string")
+  }
+  if(length(nc_lat) != 1) {
+    stop("nc_lat must be a single string")
+  }
+  if(length(nc_lon) != 1) {
+    stop("nc_lon must be a single string")
+  }
+
+  if(verbose) cat(paste0("Reading ", nc_path))
+
+  nc <- ncdf4::nc_open(nc_path)
+
+  nf1 <- grep(nc_name, names(nc$var), value = T)[1]
+
+  foot <- ncdf4::ncvar_get(nc, nf1) # here we have the dimensions
+
+  foot1lat <- ncdf4::ncvar_get(nc, nc_lat)
+
+  foot1lon <- ncdf4::ncvar_get(nc, nc_lon)
+
+  if(verbose) {
+
+    cf <- ncdf4::ncatt_get(nc, 0, "Conventions")$value
+
+    if(any(grepl("CF", cf))) print(paste0("Footprint Conventions = ", cf))
+
+  }
+  ncdf4::nc_close(nc)
+
+  return(foot)
+}
+
